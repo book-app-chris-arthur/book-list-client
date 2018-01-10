4b06ad020688279217d7e77fd0b4ce69681e5444
Chris Lesesne <clesesne3@gmail.com>
2018-01-09 19:36:35 -0800

    restructure file directory and write endpoints for single book/new book requests

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -11,3 +11,23 @@ $.get(`${__API_URL__}/api/v1/books`).then(result => {
   });
   $('#book-info').append(`<h3>Books Available: ${result.length}</h3>`);
 });
+
+const app = app || {};
+
+(function(module) {
+  const app_url = 'http://localhost:3000/api/v1/books';
+  //const app_url = 'https://cl-aa-booklist.herokuapp.com/api/v1/books';
+
+  const Book = {};
+  Book.all = [];
+
+  Book.fetchOne = function(id) {
+    return $.getJSON(`${app_url}/:${id}`).catch(err => console.error(err));
+  };
+
+  Book.create = function(book) {
+    $.post(app_url, book);
+  };
+
+  module.Book = Book;
+})(app);
